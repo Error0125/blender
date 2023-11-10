8f8a64e34d7639d9ab8a95936692386b81ce7b32
jord97@korea.edu <jord97@korea.edu>
2023-11-10 21:57:11 +0900

    __inti__.py 추가

---
+++ __init__.py
@@ -0,0 +1,15 @@
+import bpy
+import importlib
+import subprocess
+import sys
+
+bl_info = {
+    "name" : "NeuSIM",
+    "author" : "NeuSIM",
+    "description" : "Bring NEURON animations to Blender",
+    "blender" : (3, 6, 0),
+    "version" : (1, 0, 0),
+    "location" : "View3D > BlenderSpike",
+    "warning" : "",
+    "category" : "3D View"
+}
